31ef1fbffcf68107959b0ddac7ac8debb1ea2ce4
Johanna <Johanna@MERINO>
2024-12-04 13:28:07 +0100

    Initial commit Split method doesn't work as expected, try a substring later?

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> firstList = new List<int>();
+            List<int> secondList = new List<int>();
+
+            var lines = File.ReadLines("AoC-01-input.txt");
+            foreach (var line in lines)
+            {
+                // Change for something substring
+                List<string> list = line.Split("    ").ToList();
+                //int number1 = list[0];
+                firstList.Add(int.Parse(list[0]));
+                secondList.Add(int.Parse(list[1]));
+            }
+            Console.WriteLine("First list: \n");
+            Console.WriteLine(firstList);
+            Console.WriteLine("\nSecond list: ");
+            Console.WriteLine(secondList);
         }
     }
 }
