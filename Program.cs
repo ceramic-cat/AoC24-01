acebcd98ae6f79e9d0c11c416cf352eb0129f729
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2024-12-04 15:04:21 +0100

    Introduced substring Used absolute numbers to not mess up the sum Summed up all the numbers for the final sum.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC24_01
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AoC24_01
 {
     internal class Program
     {
@@ -8,18 +10,35 @@
             List<int> secondList = new List<int>();
 
             var lines = File.ReadLines("AoC-01-input.txt");
+
             foreach (var line in lines)
             {
-                // Change for something substring
-                List<string> list = line.Split("    ").ToList();
-                //int number1 = list[0];
-                firstList.Add(int.Parse(list[0]));
-                secondList.Add(int.Parse(list[1]));
+                // exempel rad: 70740   98650
+                // each number column per row turned into a int
+                int number1 = int.Parse(line.Substring(0, 5));
+                int number2 = int.Parse((line.Substring(8)));
+                // each number added to each list
+                firstList.Add(number1);
+                secondList.Add(number2);
             }
-            Console.WriteLine("First list: \n");
-            Console.WriteLine(firstList);
-            Console.WriteLine("\nSecond list: ");
-            Console.WriteLine(secondList);
+
+            // now sort the lists in ascending order
+            firstList.Sort();
+            secondList.Sort();
+
+            // a new list containing the differences in distance
+            List<int> deltaList = new List<int>();
+            for (int i = 0; i < firstList.Count; i++)
+            {
+                // used absolute to make sure that we don't get any negative numbers
+                // negative numbers would mess up our sum
+                int deltaNumber = System.Math.Abs(firstList[i] - secondList[i]);
+                // Add the number to the deltaList
+                deltaList.Add(deltaNumber);
+            }
+            // Sum up the deltaList
+            int sum = deltaList.Sum();
+            Console.WriteLine($"The sum of all distances: {sum}");
         }
     }
 }
